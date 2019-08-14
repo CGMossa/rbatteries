d7cbf0014ba7422aa0ad26a706cf06a22b112d7c
CGMossa <cgmossa@gmail.com>
2019-08-14 11:18:48 +0200

    Added upsampling to vignette.

--- R/resample.R
+++ R/resample.R
@@ -14,7 +14,12 @@
 resample <- function(x, ...) {
   dots <- list(...)
 
-  dots$n <- if (is.null(dots$n)) { length(x) } else { dots$n }
+  x_length <- length(x)
+  dots$n <- if (is.null(dots$n)) { x_length } else { dots$n }
+
+  if (dots$n >= x_length && (is.null(dots$replace) || dots$replace == FALSE)) {
+    warning("Resample (upsampling) without replacement results in NA entries")
+  }
 
   x[do.call(sample.int, dots)]
 }
