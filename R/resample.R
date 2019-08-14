e319dbb5b3a415d7c381ec83fb295617778e2b11
CGMossa <cgmossa@gmail.com>
2019-08-14 10:14:21 +0200

    Added documentation and added example.

--- R/resample.R
+++ R/resample.R
@@ -1,4 +1,16 @@
-# A function to guard against problems with sample():
+#' Re-sample
+#' A function to guard against problems with sample():
+#' @param x Vector to resample.
+#'
+#' @return Returns x with resampled entries
+#' @export
+#'
+#' @inheritDotParams base::sample.int
+#'
+#' @examples
+#' x <- 42:100
+#' resample(x)
+#'
 resample <- function(x, ...) {
-  x[sample.int(n = length(x), ...),]
+  x[sample.int(n = length(x), ...)]
 }
