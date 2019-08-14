b3477a3bfbf522db65011644e1243d3a869928a8
CGMossa <cgmossa@gmail.com>
2019-08-14 11:18:48 +0200

    Added upsampling to vignette.

--- tests/testthat/test-resample.R
+++ tests/testthat/test-resample.R
@@ -6,10 +6,16 @@ test_that("Resample returns a single element on vectors of one length", {
 
 test_that("Resample can handle up- and down-sampling", {
   x <- 1:5
-  up_sampling <- resample(x, n = 10)
+  up_sampling <- expect_warning(resample(x, n = 10))
   expect_length(up_sampling, n = 10)
 
   down_sampling <- resample(x, n = 2)
   expect_length(down_sampling, n = 2)
+})
 
+test_that("Resample results in NA when upsampling without replacements.", {
+
+  x <- 1:5
+  expect_warning(resample(x, n = 10))
+  expect_silent(resample(x, n = 10, replace = TRUE))
 })
