e05256d143593829305e501f1c3fca7bb3c9b68d
CGMossa <cgmossa@gmail.com>
2019-08-16 10:24:57 +0200

    Reverted to using @describeIn

--- R/tiles.R
+++ R/tiles.R
@@ -1,6 +1,6 @@
 #' Tiles
-#' `quartiles` returns the quartiles, `deciles` return the deciles, and `percentiles` returns the percentiles with a resolution of `0.01`.
 #'
+#' `quartiles` returns the quartiles, `deciles` return the deciles, and `percentiles` returns the percentiles with a resolution of `0.01`.
 #'
 #' @inheritParams stats::quantile
 #' @return Returns the quartiles of `x`
@@ -15,14 +15,17 @@ quartiles <-  function(x) {
 }
 
 #' @describeIn quartiles The deciles of `x`
+#'
 #' @export
 #' @examples
 #' deciles(x)
 deciles <-  function(x)
   stats::quantile(x, probs = seq(0, 1, by = 0.1))
 
-#' @describeIn quartiles The percentiles of `x`
+#' @describeIn  quartiles The percentiles of `x`
+#'
 #' @export
 #' @examples
 #' percentiles(x)
 percentiles <-  function(x)
+  stats::quantile(x, probs = seq(0, 1, by = 0.01))
