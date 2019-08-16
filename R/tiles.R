cd4a1c3ed4fccecf6b959606f33cb9516cc585fd
CGMossa <cgmossa@gmail.com>
2019-08-16 10:05:08 +0200

    Added documentation for the *tiles functions, using @describeIn. Will try something else now.

--- R/tiles.R
+++ R/tiles.R
@@ -1,8 +1,22 @@
-
-
-quartiles <-  function(x)
+#' Tiles
+#' `quartiles` returns the quartiles, `deciles` return the deciles, and `percentiles` returns the percentiles with a resolution of `0.01`.
+#'
+#'
+#' @inheritParams stats::quantile
+#' @return Returns the quartiles of `x`
+#'
+#' @export
+#'
+quartiles <-  function(x) {
   quantile(x, probs = c(0, 0.25, 0.5, 0.75, 1))
+}
+
+#' @describeIn quartiles The deciles of `x`
+#' @export
 deciles <-  function(x)
   quantile(x, probs = seq(0, 1, by = 0.1))
+
+#' @describeIn quartiles The percentiles of `x`
+#' @export
 percentiles <-  function(x)
   quantile(x, probs = seq(0, 1, by = 0.01))
